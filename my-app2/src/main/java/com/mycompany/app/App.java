abc645939ad9d438ac111bb9498181ff8fda2eec
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-07-22 03:13:11 -0300

    bye world mundial v2

--- my-app2/src/main/java/com/mycompany/app/App.java
+++ my-app2/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bye World! mundial" );
+        System.out.println( "Bye World! mundial v2" );
     }
 }
