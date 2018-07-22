aa001575949606feec97d4bd9138bea008d3d099
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-07-22 03:24:19 -0300

    hola v4

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Universal v3" );
+        System.out.println( "Hello World! Universal v4" );
     }
 }
