b2d4fc259ff173b28cffef5aae37eba499df8cb4
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-07-22 15:16:51 -0300

    hola sao paulo 2

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sao Paulo!!" );
+        System.out.println( "Hello Sao Paulo!!2" );
     }
 }
