09a3cb55acb9f3e04ffac9a1043f61f0754556bf
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-07-22 14:10:23 -0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Final Final Final! Super Final" );
+        System.out.println( "Hello World! normal " );
     }
 }
