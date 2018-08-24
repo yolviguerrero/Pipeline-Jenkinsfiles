7998a93018efe25cfcc174961353cf9fbbac3a8c
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-08-24 10:49:07 -0300

    version 5 de myapp2

--- my-app2/src/main/java/com/mycompany/app/App.java
+++ my-app2/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Adios Sao Paulo Saludos!");
+        System.out.println( "Adios Sao Paulo Saludos! v5");
     }
 }
