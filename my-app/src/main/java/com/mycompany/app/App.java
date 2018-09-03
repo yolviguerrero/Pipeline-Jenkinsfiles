269c416fc1d45ce5a784584be33528cf966d5edd
yolviguerrero <40274903+yolviguerrero@users.noreply.github.com>
2018-09-03 17:23:38 -0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sao Paulo!!!!! Saludos!!! v123" );
+        System.out.println( "Hello Sao Paulo!!!!! Salu dos!!! v123" );
     }
 }
